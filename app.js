1646d3124fe4d8c7992042635a372700e8a29250
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-26 00:00:33 -0400

    Change the loop that generates 16 x 16 tiles
    I changed the loop so that it would be apparent
    that the program is supposed to begin with
    a 16 x 16 grid, instead of wondering why they is a
    loop for 256 times.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const gridContainer = document.querySelector("#grid-container");
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("tile");
-  gridContainer.appendChild(div);
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+     const div = document.createElement("div");
+     div.classList.add("tile");
+     gridContainer.appendChild(div);
+  }
 }
