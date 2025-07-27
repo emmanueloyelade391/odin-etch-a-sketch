33ca8459e259b8a771358b9a86f4db46fd6deb67
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-26 23:10:05 -0400

    Make a function to replace the original grid
    The function receives the number of tiles the new
    grid will contain from the user and makes a new
    grid that has the same width and properties as the
    original grid. The only difference is the number
    of tiles generated.

--- app.js
+++ app.js
@@ -27,4 +27,39 @@ tileColorChanger.forEach((tile) => {
   tile.addEventListener("mouseover", (event) => {
     event.target.classList.add("modified-tile");
   })
-})
+});
+
+const gridSizeButton = document.querySelector("#grid-size-button");
+gridSizeButton.addEventListener("click", () => {
+  const userTiles = +prompt(
+    "Input the number of tiles you want per side of the grid in a range between 1 - 100:"
+  );
+  if (userTiles < 1 ) {
+    alert("Error. Please input a number that is bigger than or equal to 1.");
+  } else if (userTiles > 100) {
+    alert("Error. Please input a number that is less than 100.");
+  } else if (Number.isNaN(userTiles)) {
+    alert("Error. Please input a real number that is between 1 and 100.");
+  }
+
+  if (1 <= userTiles && userTiles <= 100) {
+    const newGridContainer = document.createElement("div");
+    newGridContainer.id = "new-grid-container";
+    gridContainer.replaceWith(newGridContainer);
+    
+    //body.appendChild(newGridContainer);
+    
+    for (let i = 0; i < userTiles; i++) {
+      for (let j = 0; j < userTiles; i++) {
+        const div = createElement("div");
+        div.style.classList.add("tile");
+        newGridContainer.appendChild(div);
+      }
+    }
+
+    const newTiles = document.querySelector(".tile");
+
+    //body.appendChild(newGridContainer);
+    console.log(newGridContainer);
+  }
+});
